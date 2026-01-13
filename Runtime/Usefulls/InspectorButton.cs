ded3e5b64613ab31b5ddd00f74d1a459918f9aad
Alice Bottino <s.trippstr@gmail.com>
2026-01-13 12:07:42 -0800

    Update InspectorButton.cs
    Updating to be in line with the HYPERBEAT repository, includes multi-object selections, editor mode options, error handling, and improved documentation.

--- Runtime/Usefulls/InspectorButton.cs
+++ Runtime/Usefulls/InspectorButton.cs
@@ -1,77 +1,166 @@
+/* NOTE:
+ *     This script should *NOT* be placed under an Editor folder in Unity, otherwise the struct would not be accessible outside 
+ * of the editor, and would fail to compile, or uses of it would need to be wrapped in #if UNITY_EDITOR blocks.        
+ */
+
+
 using System;
-using System.Reflection;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+using System.Reflection;
 #endif
+
+
+// This script is implemented through a PropertyDrawer, so as to not interfere with custom editors.
+
+
+/// <summary>
+/// An easy to use Button that can be added to MonoBehaviours and ScriptableObjects without the need of editor scripting.<br/>
+/// An implicit cast from string exists that allows for easy declaration when no additional options are needed, like so: 
+/// <code>InspectorButton button = nameof(Method);</code>
+/// </summary>
 
 [Serializable]
 public struct InspectorButton
 {
-    private string method { get; }
-    private bool shouldRecordUndo { get; }
+    [System.Flags]
+    public enum Options : byte
+    {
+        None = 0,
 
-    public InspectorButton(string nameof, bool recordUndo)
+        /// <summary> Calls Undo.RecordObject on the inspectors target objects. </summary>
+        RecordUndo = 1 << 0,
+
+        /// <summary> Disables the button when Application.isPlayer != true. </summary>
+        DisableInEditMode = 1 << 1,
+
+        /// <summary> Disables the button when Application.isPlayer != true. </summary>
+        DisableInPlayMode = 1 << 2,
+
+        /// <summary> Suspends Asset Importing while the button functions are being called. 
+        /// This is useful for if a button makes multiple file edits. </summary>
+        SuspendImporting = 1 << 3,
+    }
+
+
+    private readonly string method;
+    private readonly Options options;
+
+
+    
+
+
+    // ---- Construction ----
+
+    public InspectorButton(string nameof, Options options = Options.RecordUndo)
     {
         method = nameof;
-        shouldRecordUndo = recordUndo;
+        this.options = options;
     }
-    public static implicit operator InspectorButton(string nameof) => new InspectorButton(nameof, true);
 
+    // Implicit cast of string to InspectorButton, allows for easy declarations of buttons like so: 
+    // InspectorButton button = nameof(Method);
+    public static implicit operator InspectorButton(string nameof) => new InspectorButton(nameof);
+
+
+
+
+
+    // ---- Editor ----
 #if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(InspectorButton))]
     public class InspectorButtonDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (GUI.Button(position, label))
+
+            var target = property.serializedObject.targetObject;
+            var data = (InspectorButton)fieldInfo.GetValue(target);
+
+            bool previouslyEnabled = GUI.enabled;
+
+            if (!Application.isPlaying && data.options.HasFlag(Options.DisableInEditMode))
+                GUI.enabled &= false;
+            if (Application.isPlaying && data.options.HasFlag(Options.DisableInPlayMode))
+                GUI.enabled &= false;
+
+            bool buttonPressed = GUI.Button(position, label);
+
+            GUI.enabled = previouslyEnabled;
+
+            if (buttonPressed)
             {
-                var target = property.serializedObject.targetObject;
-                var data = (InspectorButton)fieldInfo.GetValue(target);
+                bool suspendImporting = data.options.HasFlag(Options.SuspendImporting);
 
-                var obj = property.serializedObject.targetObject;
-
-                bool isStatic = false;
-
-                var method = obj.GetType().GetMethod(data.method, BindingFlags.Public | BindingFlags.Instance);
-                if(method == null)
-                    method = obj.GetType().GetMethod(data.method, BindingFlags.NonPublic | BindingFlags.Instance);
-
-                if(method == null)
+                try
                 {
-                    isStatic = true;
+                    if (suspendImporting)
+                        AssetDatabase.StartAssetEditing();
 
-                    method = obj.GetType().GetMethod(data.method, BindingFlags.Public | BindingFlags.Static);
+                    UnityEngine.Object obj = property.serializedObject.targetObject;
+                    UnityEngine.Object[] objs = property.serializedObject.targetObjects;
+
+                    bool isStatic = false;
+
+                    MethodInfo method = obj.GetType().GetMethod(data.method, BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy); ;
                     if (method == null)
-                        method = obj.GetType().GetMethod(data.method, BindingFlags.NonPublic | BindingFlags.Static);
+                        method = obj.GetType().GetMethod(data.method, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
 
-                    if(method == null)
+
+                    if (method == null)
                     {
-                        Debug.LogError($"No function of name \"{data.method}\" was found");
+                        isStatic = true;
+
+                        method = obj.GetType().GetMethod(data.method, BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+                        if (method == null)
+                            method = obj.GetType().GetMethod(data.method, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+                        if (method == null)
+                        {
+                            Debug.LogError($"No function of name \"{data.method}\" was found.");
+                            return;
+                        }
+                    }
+
+                    if (method.GetParameters().Length > 0)
+                    {
+                        Debug.LogError("Inspector Buttons will only work on functions with no input parameters.");
                         return;
                     }
-                }
 
-                if (method.GetParameters().Length > 0)
+
+                    bool recordUndo = data.options.HasFlag(Options.RecordUndo);
+
+                    if (isStatic)
+                    {
+                        _ = method.Invoke(null, null);
+                    }
+                    else
+                    {
+                        for (int i = 0; i < objs.Length; i++)
+                        {
+                            if (recordUndo)
+                                Undo.RecordObject(objs[i], label.text);
+
+                            _ = method.Invoke(objs[i], null);
+
+                            EditorUtility.SetDirty(objs[i]);
+                        }
+                    }
+
+                    if(recordUndo)
+                        Undo.FlushUndoRecordObjects();
+                }
+                catch(Exception e)
                 {
-                    Debug.LogError("Inspector Buttons will only work on functions with no input parameters.");
-                    return;
+                    Debug.LogException(new Exception("An Exception was thrown during an InspectorButton press.", e);
                 }
-
-                if(data.shouldRecordUndo)
-                    Undo.RecordObject(target, label.text);
-
-                object methodOut = isStatic ? method.Invoke(null, null) : method.Invoke(obj, null);
-
-                if (!data.shouldRecordUndo || methodOut == null) return;
-
-                if (methodOut is UnityEngine.Object changedobj)
-                    Undo.RecordObject(changedobj, label.text);
-                else if(methodOut is UnityEngine.Object[] changedobjs)
-                    foreach (var changed in changedobjs)
-                        Undo.RecordObject(changed, label.text);
-
-                Undo.FlushUndoRecordObjects();
+                finally
+                {
+                    if (suspendImporting)
+                        AssetDatabase.StopAssetEditing();
+                }
             }
         }
     }
